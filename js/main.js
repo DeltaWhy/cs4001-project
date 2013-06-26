6d59e1ff379d52d5831e2e1d2463d2512cffa184
hammy55 <evan.cahill55@gmail.com>
2013-06-27 00:32:11 +0200

    Refactored step navigation UI and javascript controls.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
     $(".step").hide();
     $(".step1").show();
     $(".step .btn.next").bind("click", function(e) {
@@ -12,5 +12,48 @@ $(function() {
     $(".step .btn.restart").bind("click", function(e) {
         $(this).parents(".step").hide();
         $(".step2").show();
+    });
+});
+*/
+
+$(function() {
+    $(".step").hide();
+    $(".step-controls .btn.restart").hide();
+    $(".step-controls").hide();
+    $(".step:first").show();
+    
+    //initial next button
+    $(".step .btn.next").bind("click", function(e) {
+        $(this).parents(".step").hide();
+        $(this).parents(".step").next().show();
+        $(".step-controls").show();
+    });
+    
+    //next button
+    $(".step-controls .btn.next").bind("click", function(e) {
+        var $step = $(".step:visible").hide().next().show();
+        if ($step.is(".step:last")) {
+            $(".step-controls .btn.next").hide();
+            $(".step-controls .btn.restart").show();
+        }
+    });
+    
+    //previous button
+    $(".step-controls .btn.prev").bind("click", function(e) {
+        var $step = $(".step:visible").hide().prev().show();
+        if ($step.is(".step:first")) {
+            $(".step-controls").hide();
+        } else if ($step.next().is(".step:last")) {
+            $(".step-controls .btn.restart").hide();
+            $(".step-controls .btn.next").show();
+        }
+    });
+    
+    //reset button
+    $(".step-controls .btn.restart").bind("click", function(e) {
+        $(".step:visible").hide();
+        $(".step").eq(1).show();
+        $(".step-controls .btn.restart").hide();
+        $(".step-controls .btn.next").show();
     });
 });

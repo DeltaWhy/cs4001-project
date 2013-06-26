27516296b743d2fb2a6ce7c4592d7ace34122ddf
Michael Limiero <mike5713@gmail.com>
2013-06-26 22:13:12 +0200

    off to see the wizard

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+$(function() {
+    $(".step").hide();
+    $(".step1").show();
+    $(".step .btn.next").bind("click", function(e) {
+        $(this).parents(".step").hide();
+        $(this).parents(".step").next().show();
+    });
+    $(".step .btn.prev").bind("click", function(e) {
+        $(this).parents(".step").hide();
+        $(this).parents(".step").prev().show();
+    });
+    $(".step .btn.restart").bind("click", function(e) {
+        $(this).parents(".step").hide();
+        $(".step2").show();
+    });
+});

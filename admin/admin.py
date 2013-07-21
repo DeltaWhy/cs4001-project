0127530a0d156176e9217f1a4dc235e167d45046
Michael Limiero <mike5713@gmail.com>
2013-07-21 14:12:37 +0200

    Set up static routing for development

--- admin/admin.py
+++ admin/admin.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
-from bottle import route, run
+from bottle import route, run, static_file
 
-@route('/')
+if __name__ == '__main__':
+    base = '/admin'
+else:
+    base = ''
+
+@route(base)
+@route(base+'/')
 def hello():
     return 'ello admin'
 
+@route(base+'/<filename:re:(css|img|js)/.*>')
+def admin_static(filename):
+    return static_file(filename, root='.')
+
 if __name__ == '__main__':
-    run(host='localhost', port=8080, debug=True)
+    @route('/')
+    @route('/<filename:re:(css|img|js)/.*>')
+    def static(filename='index.html'):
+        return static_file(filename, root='..')
+
+
+    run(host='localhost', port=8080, debug=True, reloader=True)
